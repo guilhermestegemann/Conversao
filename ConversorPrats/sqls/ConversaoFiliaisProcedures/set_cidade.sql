31200b484982c105e5ea0b48120ed482a4108bf7
guilhermestegemann <guilherme.stegemann@gmail.com>
2019-09-04 10:03:17 -0300

    .

---
+++ ConversorPrats/sqls/ConversaoFiliaisProcedures/set_cidade.sql
@@ -0,0 +1,46 @@
+SET TERM ^ ;
+
+create or alter procedure set_cidade_conv (
+    nome varchar(40),
+    estado integer,
+    codigofiscal varchar(7),
+    populacao integer)
+as
+declare variable codigocidade integer;
+BEGIN
+    SELECT CIDADE.CODIGO
+    FROM CIDADE
+    WHERE CIDADE.CODIGOFISCAL = :CODIGOFISCAL
+    INTO :CODIGOCIDADE;
+    IF (:CODIGOCIDADE IS NULL) THEN
+    BEGIN
+        SELECT RETORNO
+        FROM GET_CODIGO('CIDADE')
+        INTO :CODIGOCIDADE;
+        INSERT INTO CIDADE
+        (
+            CODIGO,
+            NOME,
+            POPULACAO,
+            REGIAOVENDA,
+            COMISSAOENTREGA,
+            ESTADO,
+            VALORFRETEADICIONAL,
+            CODIGOFISCAL
+        ) VALUES
+        (
+            :CODIGOCIDADE,
+            :NOME,
+            :POPULACAO,
+            'S',
+            0,
+            :ESTADO,
+            0,
+            :CODIGOFISCAL
+        );
+            
+    END
+END^
+
+SET TERM ; ^
+
